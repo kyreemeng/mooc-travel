3274b7f15e5401b99a0836101b248415d6fcc0c2
dreamhao <idreamhao@gmail.com>
2019-11-16 00:37:06 +0800

    change

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for=" item of swiperList " :key="item.id">
+        <img
+          class="swiper-img"
+          :src="item.imgUrl"
+        />
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeSwiper",
+  data() {
+    return {
+      swiperOption: {
+        pagination: ".swiper-pagination",
+       loop:true,
+      },
+      swiperList:[{
+          id:'001',
+          imgUrl:'//source.qunarzz.com/site/images/wns/20191101_qunar_dujia_750x192_3.jpg'
+      },{
+          id:'002',
+          imgUrl:'//source.qunarzz.com/site/images/wns/20191113_qunar_dujia_750x192_2.jpg'
+      }],
+    };
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+.wrapper >>> .swiper-pagination-bullet-active {
+  background: #fff;
+}
+
+.wrapper {
+  overflow: hidden;
+  width: 100%;
+  height: 0;
+  padding-bottom: 32%;
+
+  .swiper-img {
+    width: 100%;
+  }
+}
+</style>
