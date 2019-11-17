8ad4e52a82179ba6481dba5d9cbab5e87c6b7faf
dreamhao <idreamhao@gmail.com>
2019-11-18 00:27:41 +0800

    city-router

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -8,10 +8,13 @@
            输入城市/景点/游玩主题
 
        </div>
-       <div class="header-right">
+       <router-link to="/city">
+              <div class="header-right">
           {{city}}
             <span class="iconfont arrow-icon">&#xe64a;</span>
        </div>
+       </router-link>
+    
     </div>
 </template>
 
@@ -29,7 +32,7 @@ export default {
 @import '~styles/varibles.styl'
     .header
         display:flex
-        line-height: .86rem
+        line-height: $headerHeight
         color:#fff
         background-color: $bgColor
         .header-left
@@ -38,21 +41,22 @@ export default {
             .back-icon
                 text-align:center
                 font-size:.4rem
-         .header-input
-                flex:1
-                height:.64rem
-                line-height:.64rem
-                margin-top: .12rem
-                margin-left :.2rem
-                padding-left:.2rem
-                background-color: #fff
-                border-radius : .1rem
-                color:#ccc
-            .header-right
-                 width :1.24rem
-                 float:right
-                 text-align:center
-                .arrow-icon
-                    margin-left : -.04rem
-                    font-size:.24rem
+        .header-input
+            flex:1
+            height:.64rem
+            line-height:.64rem
+            margin-top: .12rem
+            margin-left :.2rem
+            padding-left:.2rem
+            background-color: #fff
+            border-radius : .1rem
+            color:#ccc
+        .header-right
+            color:#fff
+            width :1.24rem
+            float:right
+            text-align:center
+            .arrow-icon
+                margin-left : -.04rem
+                font-size:.24rem
 </style>
