6d7a2745943b524051c29ce50f53cb60d5ae4a10
dreamhao <idreamhao@gmail.com>
2019-11-20 22:26:01 +0800

    1120

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,12 +1,50 @@
 <template>
+<div>
     <div class="search">
-        <input class="search-input" type="text" placeholder="输入城市名或拼音" />
+        <input  v-model="keyword" class="search-input" type="text" placeholder="输入城市名或拼音" />
     </div>
+    <div class="search-content">
+        <ul>
+            <li class="search-item border-bottom " v-for="(item,index) of list" :key="index">{{item.name}}</li>
+        </ul>
+    </div>
+    </div>
+    
 </template>
 
 <script>
 export default {
-    name: 'CitySearch'
+    name: 'CitySearch',
+    props:{
+        cities:Object
+    },
+    data() {
+        return {
+            keyword:'',
+            list:[],
+            timer:null
+        }
+    },
+    watch:{
+        keyword(){
+            if(this.timer){
+                clearTimeout(this.timer)
+            }
+            this.timer = setTimeout(()=>{
+                const result = []
+                for (let i in this.cities) {
+                    this.cities[i].forEach((value)=>{
+                        if (value.spell.indexOf(this.keyword)> -1 || value.name.indexOf(this.keyword)> -1) {
+                            result.push(value)
+                        }
+
+                    })
+                }
+                this.list = result;
+            },100)
+
+        }
+    }
 }
 </script>
 
@@ -25,5 +63,18 @@ export default {
             text-align:center
             border-radius :0.06rem
             color:#666
-     
+    .search-content
+        z-index:1
+        overflow:hidden
+        position:absolute
+        top:1.58rem
+        left:0
+        right:0
+        bottom:0
+        background:#eee
+    .search-item
+        line-height:0.62rem
+        padding-left:0.2rem
+        background:#fff
+        color:#666
 </style>
