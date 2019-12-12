1b99e5ccedbe9968635b4749b59b2e71c9853828
dreamhao <idreamhao@gmail.com>
2019-12-12 16:56:57 +0800

    finish-detail-list

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -2,19 +2,47 @@
     <div>
         <DetailBanner></DetailBanner>
         <DetailHeader></DetailHeader>
-        <div class="content" ></div>
+        <div class="content" >
+            <DetailList :list="list"></DetailList>
+        </div>
     </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner'
 import DetailHeader from './components/Header'
+import DetailList from './components/List'
 export default {
     name:'Detail',
     components:{
         DetailBanner,
         DetailHeader,
-    }
+        DetailList,
+    },
+    data() {
+        return {
+            list:[{
+                title:'成人票',
+                children:[{
+                    title:'成人票场馆联票',
+                     children:[{
+                    title:'成人票场馆联票-红公馆',
+                }]
+                    
+                },{
+                    title:'成人票2'
+                },{
+                    title:'成人票3'
+                },]
+            },{
+                title:'学生票'
+            },{
+                title:'儿童票'
+            },{
+                title:'特惠票'
+            }]
+        }
+    },
 }
 </script>
 
